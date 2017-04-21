b743d41b4d828923fd288b0e7cb5b258920af166
eposasdieste <eposas@dieste.com>
2017-04-20 20:29:34 -0500

    edited config --shortened name

--- MAIN/config.js
+++ MAIN/config.js
@@ -1,5 +1,5 @@
 var config = {
-    title : 'DTV31231-DTV-Pre',
+    title : 'DTV31231',
     sections : ['SPANISH', 'ENGLISH'],
     ad_sizes : [ '160x600', '300x250', '300x600', '320x50', '728x90', '970x250' ],
     download_zip_enabled : false
